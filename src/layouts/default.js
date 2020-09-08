1b00a50daf976810742694f01489a90c9cb9d8db
Carsten Rodin <carsten.rodin@gmail.com>
2020-09-07 20:15:53 -0400

    adds simple window.prompt based password authentication

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Header from '../components/header'
-import Nav from '../components/nav'
-import '../style/main.css'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../components/header';
+import Nav from '../components/nav';
+import '../style/main.css';
 
 const Layout = ({ children, siteMetadata }) => {
-    return(
+    const password = 'gis2020';
+    const [enteredPassword, setEnteredPassword] = useState();
+
+    useEffect(() => {
+      var storedPassword = localStorage.getItem('pw');
+      if (storedPassword != password) {
+        var pass = prompt("Enter password");
+        setEnteredPassword(pass);
+        localStorage.setItem('pw', pass);
+      } else {
+        setEnteredPassword(storedPassword);
+      }
+    }, []);
+
+    if (enteredPassword === password) {
+      return(
         <div id='page-container' className='grid grid-cols-6 gap-8 p-8 text-md min-h-full'>
           <Header siteMetadata={siteMetadata} />
           <Nav />
@@ -13,7 +28,13 @@ const Layout = ({ children, siteMetadata }) => {
             {children}
           </div>
         </div>
-    )
+      )
+    } else {
+      return(
+        <div className='blank-login'/>
+      )
+    }
+
 }
 
 Layout.propTypes = {
